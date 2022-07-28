379cdf601d53f430dc53c598fa816009df0daf6f
James Alt <79052050+imacj@users.noreply.github.com>
2022-07-28 02:48:02 -0400

    add demo files

---
+++ bouncing_cubes.py
@@ -0,0 +1,36 @@
+"""
+Bouncing Cubes animation script
+For MIDIAnimator version 1.0
+Licensed under the GNU General Public License v3.0
+
+Notes:
+Run script by pressing the "Run Script" button.
+It will take 18-20 seconds to process. Be patient while MIDIAnimator processes the MIDI file & animates it.
+"""
+
+import bpy
+from MIDIAnimator.data_structures.midi import MIDIFile
+from MIDIAnimator.src.animation import MIDIAnimatorNode
+
+# Get MIDI File
+file = MIDIFile("//mapleaf.mid")
+
+# Find both tracks and merge them
+lh = file.findTrack('Piano - L.H.')
+rh = file.findTrack('Piano - R.H.')
+piano = file.mergeTracks(lh, rh, name="Piano")
+
+# Assigning notes to objects
+# You can also handle this in the 3D Viewport MIDAnimator menu.
+scene = bpy.context.scene
+scene.quick_instrument_type = "string"
+scene.note_number_list = str(piano.allUsedNotes())
+scene.quick_obj_col = bpy.data.collections['Cubes']
+scene.quick_obj_curve = bpy.data.objects['ANIM_Osc']
+scene.quick_use_sorted = True
+bpy.ops.scene.quick_add_props()
+
+# create a MIDIAnimatorNode object & add tracks
+animator = MIDIAnimatorNode()
+animator.addInstrument(midiTrack=piano, objectCollection=bpy.data.collections['Cubes'])
+animator.animate()
